6eecebc8c8fc29f635d7e7aefdb9b136915f9bee
huangyt39 <824551061@qq.com>
2018-06-20 11:23:05 +0800

    Homework5

--- Homework5/Classes/Thunder.cpp
+++ Homework5/Classes/Thunder.cpp
@@ -23,17 +23,16 @@ bool Thunder::init() {
 	if (!Layer::init()) {
 		return false;
 	}
-	stoneType = 0;
+	stoneType = 1;
 	isMove = false;  // 是否点击飞船
 	visibleSize = Director::getInstance()->getVisibleSize();
 
 	// 创建背景
-	/*
 	auto bgsprite = Sprite::create("bg.jpg");
 	bgsprite->setPosition(visibleSize / 2);
 	bgsprite->setScale(visibleSize.width / bgsprite->getContentSize().width,
 		visibleSize.height / bgsprite->getContentSize().height);
-	this->addChild(bgsprite, 0);*/
+	this->addChild(bgsprite, 0);
 
 	// 创建飞船
 	player = Sprite::create("player.png");
@@ -70,14 +69,14 @@ bool Thunder::init() {
 
 //预加载音乐文件
 void Thunder::preloadMusic() {
-	// Todo
-
+	SimpleAudioEngine::getInstance()->preloadBackgroundMusic("music/bgm.mp3");
+	SimpleAudioEngine::getInstance()->preloadEffect("music/explore.wav");
+	SimpleAudioEngine::getInstance()->preloadEffect("music/fire.wav");
 }
-
 //播放背景音乐
 void Thunder::playBgm() {
 	// Todo
-
+	SimpleAudioEngine::getInstance()->playBackgroundMusic("music/bgm.mp3", true);
 }
 
 //初始化陨石
@@ -102,12 +101,41 @@ void Thunder::addEnemy(int n) {
 // 陨石向下移动并生成新的一行(加分项)
 void Thunder::newEnemy() {
 	// Todo
+	for each (auto item in this->enemys)
+	{
+		item->setPosition(item->getPositionX(), item->getPositionY() - 50);
+	}
+	//new line
+	char enemyPath[20];
+	sprintf(enemyPath, "stone%d.png", 3 - stoneType);
+	double width = visibleSize.width / 6.0,
+		height = visibleSize.height - 50;
+	for (int j = 0; j < 5; ++j) {
+		auto enemy = Sprite::create(enemyPath);
+		enemy->setAnchorPoint(Vec2(0.5, 0.5));
+		enemy->setScale(0.5, 0.5);
+		enemy->setPosition(width * (j + 1) - 70, height);
+		enemys.push_back(enemy);
+		addChild(enemy, 1);
+	}
+	stoneType = (stoneType + 1) % 3;
 }
 
 // 移动飞船
 void Thunder::movePlane(char c) {
 	// Todo
-
+	auto nowPos = player->getPosition();
+	float newX;
+	switch ((int)c) {
+	case 'A' : case 26:		//A ←
+		newX = player->getPositionX() - 20 > 20 ? player->getPositionX() - 20 : 20;
+		player->runAction(MoveTo::create(0.2f, Vec2(newX, player->getPositionY())));
+		break;
+	case 'D' : case 27:		//D →
+		newX = player->getPositionX() + 20 > visibleSize.width - 20 ? visibleSize.width - 20 : player->getPositionX() + 20;
+		player->runAction(MoveTo::create(0.2f, Vec2(newX, player->getPositionY())));
+		break;
+	}
 }
 
 //发射子弹
@@ -121,13 +149,23 @@ void Thunder::fire() {
 
 	// 移除飞出屏幕外的子弹
 	// Todo
+	bullet->runAction(Sequence::create(MoveBy::create(1.0f, Vec2(0, visibleSize.height)), CallFunc::create([this, bullet]() {
+		bullet->removeFromParentAndCleanup(true);
+		this->bullets.remove(bullet);
+	}), nullptr));   
 
 }
 
 // 切割爆炸动画帧
 void Thunder::explosion() {
 	// Todo
-
+	auto exptex = Director::getInstance()->getTextureCache()->addImage("explosion.png");
+	for (int i = 0; i < 5; i++) {
+		explore.pushBack(SpriteFrame::createWithTexture(exptex, CC_RECT_PIXELS_TO_POINTS(Rect(20 + i*190, 0, 140, 140))));
+	}
+	for (int i = 0; i < 3; i++) {
+		explore.pushBack(SpriteFrame::createWithTexture(exptex, CC_RECT_PIXELS_TO_POINTS(Rect(20 + i * 190, 190, 140, 140))));
+	}
 }
 
 void Thunder::update(float f) {
@@ -172,6 +210,39 @@ void Thunder::update(float f) {
 void Thunder::meet(EventCustom * event) {
 	// 判断子弹是否打中陨石并执行对应操作
 	// Todo
+	for each (auto enemy in this->enemys)
+	{
+		//judge if game over
+		if (enemy->getBoundingBox().getMinY() <= this->player->getBoundingBox().getMaxY()) {
+			auto tmp = this->player;
+			player->runAction(Sequence::create(Animate::create(Animation::createWithSpriteFrames(explore, 0.05f, 1)), CallFunc::create([this, tmp] {
+				tmp->removeFromParentAndCleanup(true);
+				//gameover pic
+				auto gameover = Sprite::create("gameOver.png");
+				gameover->setPosition(visibleSize / 2);
+				this->addChild(gameover, 1);
+			}), nullptr));
+			this->getEventDispatcher()->removeAllEventListeners();
+			SimpleAudioEngine::getInstance()->playEffect("music/explore.wav", false);
+			unschedule(schedule_selector(Thunder::update));
+			return;
+		}
+		//judge if meet
+		for each (auto bullet in this->bullets)
+		{
+			if (enemy->getPosition().getDistance(bullet->getPosition()) < 20) {
+				auto temp = enemy;
+				bullet->removeFromParentAndCleanup(true);
+				enemy->runAction(Sequence::create(Animate::create(Animation::createWithSpriteFrames(explore, 0.05f, 1)), CallFunc::create([this, temp] {
+					temp->removeFromParentAndCleanup(true);
+				}), nullptr));
+				SimpleAudioEngine::getInstance()->playEffect("music/explore.wav", false);
+				bullets.remove(bullet);
+				enemys.remove(enemy);
+				return;
+			}
+		}
+	}
 }
 
 
@@ -188,13 +259,17 @@ void Thunder::stopAc() {
 // 添加自定义监听器
 void Thunder::addCustomListener() {
 	// Todo
-
+	auto meetListener = EventListenerCustom::create("meet", CC_CALLBACK_1(Thunder::meet, this));
+	_eventDispatcher->addEventListenerWithFixedPriority(meetListener, 1);
 }
 
 // 添加键盘事件监听器
 void Thunder::addKeyboardListener() {
 	// Todo
-
+	auto keyListener = EventListenerKeyboard::create();
+  keyListener->onKeyPressed = CC_CALLBACK_2(Thunder::onKeyPressed, this);
+  keyListener->onKeyReleased = CC_CALLBACK_2(Thunder::onKeyReleased, this);
+  this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(keyListener, this);
 }
 
 void Thunder::onKeyPressed(EventKeyboard::KeyCode code, Event* event) {
@@ -233,7 +308,12 @@ void Thunder::onKeyReleased(EventKeyboard::KeyCode code, Event* event) {
 // 添加触摸事件监听器
 void Thunder::addTouchListener() {
 	// Todo
-
+	this->setTouchEnabled(true);
+	auto touchListener = EventListenerTouchOneByOne::create();
+	touchListener->onTouchBegan = CC_CALLBACK_2(Thunder::onTouchBegan, this);
+	touchListener->onTouchMoved = CC_CALLBACK_2(Thunder::onTouchMoved, this);
+	touchListener->onTouchEnded = CC_CALLBACK_2(Thunder::onTouchEnded, this);
+	this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(touchListener, this);
 }
 
 // 鼠标点击发射炮弹
@@ -241,8 +321,8 @@ bool Thunder::onTouchBegan(Touch *touch, Event *event) {
 	if (touch->getLocation().getDistance(player->getPosition()) <= 30)
         isClick = true;
     // Todo
+	fire();
 	return true;
-
 }
 
 void Thunder::onTouchEnded(Touch *touch, Event *event) {
@@ -252,5 +332,10 @@ void Thunder::onTouchEnded(Touch *touch, Event *event) {
 // 当鼠标按住飞船后可控制飞船移动 (加分项)
 void Thunder::onTouchMoved(Touch *touch, Event *event) {
 	// Todo
-
+	if (isClick) {
+		float x = touch->getDelta().x + player->getPositionX();
+		if (x < 0) x = 0;
+		if (x > visibleSize.width) x = visibleSize.width;
+		player->setPosition(x, player->getPositionY());
+	}
 }
